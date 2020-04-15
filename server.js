3952ac4f19aa5bcd5eb1b0def0b88660230136f2
Aphisitt308 <bam52170@gmail.com>
2020-04-15 14:00:35 +0700

    f3

--- server.js
+++ server.js
@@ -18,21 +18,7 @@ app.use(cors({
    origin: 'https://damp-beyond-98873.herokuapp.com'
 }));
 
-const allowedOrigins = ['http://localhost:3000',
-                      'https://damp-beyond-98873.herokuapp.com'];
-app.use(cors({
-  origin: function(origin, callback){
-    // allow requests with no origin 
-    // (like mobile apps or curl requests)
-    if(!origin) return callback(null, true);
-    if(allowedOrigins.indexOf(origin) === -1){
-      var msg = 'The CORS policy for this site does not ' +
-                'allow access from the specified Origin.';
-      return callback(new Error(msg), false);
-    }
-    return callback(null, true);
-  }
-}));
+
 
 //middleware
 app.use(bodyParser.urlencoded({ extended: false }));
